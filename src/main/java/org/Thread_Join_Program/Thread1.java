824f98de255ba7c6ff1f6b3f70e0be330fb6534c
Ravi kant <118505126+Officialravi888@users.noreply.github.com>
2022-12-30 18:47:04 +0530

    Update Thread1.java

--- src/main/java/org/Thread_Join_Program/Thread1.java
+++ src/main/java/org/Thread_Join_Program/Thread1.java
@@ -1,4 +1,14 @@
-package org.Thread_Join_Program;
-
-public class Thread1 {
-}
+class TestDaemonThread2 extends Thread{  
+ public void run(){  
+  System.out.println("Name: "+Thread.currentThread().getName());  
+  System.out.println("Daemon: "+Thread.currentThread().isDaemon());  
+ }  
+  
+ public static void main(String[] args){  
+  TestDaemonThread2 t1=new TestDaemonThread2();  
+  TestDaemonThread2 t2=new TestDaemonThread2();  
+  t1.start();  
+  t1.setDaemon(true);//will throw exception here  
+  t2.start();  
+ }  
+}  
