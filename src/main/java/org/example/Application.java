1e897a4bdc8dbe7167cd0b5412e1593a5ac97b19
Officialravi888 <youremail@yourdomain.com>
2022-12-30 16:12:48 +0530

    Executor File

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -1,23 +1,33 @@
-package org.example;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class Application {
-    public void great(){
-        List<String> list=new ArrayList<>();
-        list.add("Hello");
-        for(String list1 : list){
-            System.out.println("list1:"+list1);
-        }
-    }
-    public Application(){
-        System.out.println("Inside Application");
-    }
-
-    public static void main(String[] args) {
-        System.out.println("Starting Application");
-        Application app = new Application();
-        app.great();
-    }
-}
+//package org.example;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//import org.apache.commons.lang3.Stringutils;
+//public class Application {
+//    public int countWords(String words) {
+//        String[] separatelords = StringUtils.split(words, ' ');
+//        return (separateWords —null) ?0 :separateWords.length;
+//    }
+//    public void great(){
+//        List<String> list=new ArrayList<>();
+//        list.add("Hello");
+//        for(String list1 : list){
+//            System.out.println("list1:"+list1);
+//        }
+//    }
+//    public Application(){
+//        System.out.println("Inside Application");
+//    }
+//
+//    public static void main(String[] args) {
+//        System.out.println("Starting Application");
+//        Application app = new Application();
+//        app.great();
+//        int count = app.countlords("I have four words");
+//        System.out.println("Word Count: " + count);
+//    }
+//
+//    private int countlords(String iHaveFourWords) {
+//        return 0;
+//    }
+//}
